380d270fa651e51d6c5b6ad75360a8eed640c2d0
Pedro Jorge <pedro25salomao@gmail.com>
2019-10-15 18:18:12 -0300

    home terminada

--- src/app/usuario/home/home.component.ts
+++ src/app/usuario/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EventoService } from '../../servisos/evento.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	eventos;
+
+  constructor(private eventoServe: EventoService) { }
 
   ngOnInit() {
+  	this.liste();
+  }
+
+  liste(){
+  	this.eventoServe.listar("")
+  		.subscribe(dados => {
+        this.eventos = this.filtroPalavra(dados);
+      });
+  }
+
+  filtroPalavra(dados){
+  	console.log(dados)
+  	var list=[];
+  	for (var i = 4; i >= 0; i--) {
+  		var e = dados.pop();
+  		if(e!=null)
+  			list.push(e);
+  	}
+  	return list;
   }
 
 }
